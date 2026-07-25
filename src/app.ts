6a21bbee7984554b67898015f3715afb3ffc28b8
Kelvin Ochubili <kelvinochubili@gmail.com>
2026-07-25 09:04:55 +0100

    chore: generate prisma client and add it to fastify plugins

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,10 @@
 import Fastify from 'fastify';
 import healthRoute from './routes/health.js';
+import prismaPlugin from './plugins/prisma.js';
 
 const app = Fastify({ logger: true });
 
+app.register(prismaPlugin);
 app.register(healthRoute);
 
 export default app;
